0c23848c19d93a494679c23a37355ef46ccbc2a5
Carl <cjosephpaguia@gmail.com>
2021-10-01 00:20:37 +0800

    rewrote exercise 2.14 (countrydata) based on solution. ignore commit and see 1 previous commit for actual submission of countrydata.

--- part2/countrydata/src/App.js
+++ part2/countrydata/src/App.js
@@ -5,140 +5,140 @@ const App = () => {
 
   const [countries, setCountries] = useState([])
   const [search, setSearch] = useState('')
-  const [weatherData, setWeatherData] = useState({})
-  const api_key = process.env.REACT_APP_API_KEY
 
-  const fetchCountries = () => {
+  useEffect(() => {
     axios
       .get(`https://restcountries.com/v3.1/all`)
       .then(response => {
         setCountries(response.data)
       })
-  }
-
-  useEffect(fetchCountries, [])
-
-  console.log(countries);
-
-  const countriesToShow = search ? countries.filter(country =>
-    country.name.common.toLowerCase().search(search.toLowerCase()) !== -1) : countries
-
+  }, [])
+  
   const handleSearch = (event) => {
     setSearch(event.target.value)
     console.log(search);
   }
-
+  
   const handleShowEvent = (index) => {
     setSearch(countriesToShow[index].name.common)
   }
 
+  const countriesToShow = search ? countries.filter(country =>
+    country.name.common.toLowerCase().search(search.toLowerCase()) !== -1) : countries
+
+  console.log(countriesToShow);
+
   return (
     <div>
       <h3>data found in https://restcountries.com/v3.1/all</h3>
-      <div>
-        filter by country name: <input value={search} onChange={handleSearch} />
-      </div>
-      <Display
+      <Search search={search} eventHandler={handleSearch} />
+      <Countries
         countriesToShow={countriesToShow}
-        search={search}
         showHandler={handleShowEvent}
-        weatherData={weatherData}
-        setWeatherData={setWeatherData}
-        api_key={api_key}
       />
     </div>
   );
 }
 
-const Display = ({ countriesToShow, search, showHandler, weatherData, setWeatherData, api_key }) => {
+const Search = ({ search, eventHandler }) => {
+  return (
+    <div>
+      filter by country name: <input value={search} onChange={eventHandler} />
+    </div>
+  )
+}
 
-  // useEffect(() =>
-  //   axios
-  //     .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${countriesToShow[0].name.common}`)
-  //     .then(response => {
-  //       setWeatherData(response.data)
-  //     }), [search])
-  // console.log(weatherData);
+const Weather = ({ weatherData, city }) => {
+  console.log(weatherData);
+  if (!weatherData) {
+    return null
+  }
 
-  if (1 < countriesToShow.length && countriesToShow.length <= 10 && search) {
+  return (
+    <div>
+      <h3>Weather in {city}</h3>
+      <p>temperature: {weatherData.current.temperature} degrees Celsius</p>
+      <img
+        src={weatherData.current.weather_icons[0]}
+        alt={`weather visual for ${city}`}
+      />
+      <p>wind: {weatherData.current.wind_speed} mph direction {weatherData.current.wind_dir}</p>
+    </div>
+  )
+}
+
+const Country = ({ countryObject }) => {
+
+  const [weatherData, setWeatherData] = useState(null)
+  const api_key = process.env.REACT_APP_API_KEY
+
+  useEffect(() =>
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${countryObject.capital[0]}`)
+      .then(response => {
+        console.log(response);
+        setWeatherData(response.data)
+      }), [])
+
+  const lang = []
+  for (const eachLang in countryObject.languages) {
+    lang.push(countryObject.languages[eachLang]);
+  }
+
+  return (
+    <div>
+      <h2>{countryObject.name.common}</h2>
+      <p>capital: {countryObject.capital[0]}</p>
+      <p>area: {countryObject.area} square kilometers</p>
+      <h3>languages</h3>
+      <ul>
+        {lang.map((n, i) =>
+          <Language key={i} indivLang={n} />
+        )}
+      </ul>
+      <img
+        src={countryObject.flags.svg}
+        alt={`flag of ${countryObject.name.common}`}
+        height='150' />
+      <Weather weatherData={weatherData} city={countryObject.capital[0]}/>
+    </div>
+  )
+}
+
+const Countries = ({ countriesToShow, showHandler }) => {
+  
+  if (countriesToShow.length === 0) {
+    return (
+      <div>
+        No matches.
+      </div>
+    )
+  }
+
+  if (countriesToShow.length === 1) {
+    console.log(countriesToShow);
+    return (
+      <Country countryObject={countriesToShow[0]} />
+    )
+  }
+
+  if (countriesToShow.length < 10) {
     return (
       <div>
         {countriesToShow.map((country, i) =>
-          <Country
-            key={i}
-            country={country}
-            eventHandler={() => showHandler(i)}
-          />
+          <p key={i}>
+            {country.name.common}
+            <button onClick={() => showHandler(i)}>show</button>
+          </p>
         )}
-      </div>
-    )
-  }
-
-  else if (countriesToShow.length > 10 && search) {
-    return (
-      <div>
-        Too many matches, specify further.
-      </div>
-    )
-  }
-
-  else if (countriesToShow.length === 1 && search) {
-
-    const countryObject = countriesToShow[0]
-
-    const lang = []
-    for (const eachLang in countryObject.languages) {
-      lang.push(countryObject.languages[eachLang]);
-    }
-
-    return (
-      <div>
-        <h2>{countryObject.name.common}</h2>
-        <p>capital: {countryObject.capital[0]}</p>
-        <p>area: {countryObject.area} square kilometers</p>
-        <h3>languages</h3>
-        <ul>
-          {lang.map((n, i) =>
-            <Language key={i} indivLang={n} />
-          )}
-        </ul>
-        <img
-          src={countryObject.flags.svg}
-          alt={`flag of ${countryObject.name.common}`}
-          height='150' />
-        <h3>Weather in {countryObject.name.common}</h3>
-        {/* <p>temperature: {weatherData.current.temperature} degrees Celsius</p>
-        <img
-          src={weatherData.current.weather_icons[0]}
-          alt={`weather visual for ${countryObject.name.common}`}
-        />
-        <p>wind: {weatherData.current.wind_speed} mph direction {weatherData.current.wind_dir}</p> */}
-      </div>
-    )
-
-  }
-
-  else if (countriesToShow.length === 0 && search) {
-    return (
-      <div>
-        No countries found.
       </div>
     )
   }
 
   return (
     <div>
-      Search field empty or data still being fetched.
+      Too many matches, specify another filter
     </div>
-  )
-}
-
-const Country = ({ country, eventHandler }) => {
-  return (
-    <p>
-      {country.name.common}
-      <button onClick={eventHandler}>show</button>
-    </p>
   )
 }
 

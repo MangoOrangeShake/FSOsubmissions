72953ae8b118bd87799d51ffc353466e8c27e8df
Carl <cjosephpaguia@gmail.com>
2021-11-10 21:27:41 +0800

    4.2 complete (controllers and models)

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,19 +1,11 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 logger.info('connecting...') 
 
@@ -29,23 +21,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
